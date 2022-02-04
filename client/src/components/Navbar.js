4bfd44436dc1425411d0ffb14b9cc6e33608e11f
goyal <goyalhimanshu079@gmail.com>
2022-02-04 14:16:14 +0530

    sdf

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,47 +15,45 @@ function NavBar() {
   };
   return (
     //something wrong with navbar width
-    <div className="container" style={{ backgroundColor: "#F9F1F0" }}>
-      <div>
-        <div className="navbar">
-          <div className="logo">
-            <Link to="/">
-              <img src="../images/logo.png" alt="logo" width="125px" />
-            </Link>
-          </div>
-          <nav>
-            <ul
-              id="MenuItems"
-              style={toggle ? { maxHeight: "200px" } : { maxHeight: "0px" }}
-            >
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/cart">Cart</Link>
-              </li>
-              {userInfo ? (
-                <li>
-                  <Link to="/login" onClick={logoutHandler}>
-                    Logout
-                  </Link>
-                </li>
-              ) : (
-                <li>
-                  <Link to="/login">Sign In</Link>
-                </li>
-              )}
-            </ul>
-          </nav>
+    <div className="container">
+      <div className="navbar">
+        <div className="logo">
           <Link to="/">
-            <img src="../images/cart.png" width="30px" height="30px" />
+            <img src="../images/logo.png" alt="logo" width="125px" />
           </Link>
-          <img
-            src="../images/menu.png"
-            className="menu-icon"
-            onClick={menuToggle}
-          />
         </div>
+        <nav>
+          <ul
+            id="MenuItems"
+            style={toggle ? { maxHeight: "200px" } : { maxHeight: "0px" }}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+            {userInfo ? (
+              <li>
+                <Link to="/login" onClick={logoutHandler}>
+                  Logout
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to="/login">Sign In</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+        <Link to="/">
+          <img src="../images/cart.png" width="30px" height="30px" />
+        </Link>
+        <img
+          src="../images/menu.png"
+          className="menu-icon"
+          onClick={menuToggle}
+        />
       </div>
     </div>
   );
